dc89883adf758c74644cc56832c35d7ec32d86a7
Arrick D <arrickdunlap@gmail.com>
2021-10-11 23:16:32 +0000

    fixed error

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,4 @@
-class Micropost < ApplicationRecord::Base
+class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, length: { maximum: 140 }
-  validates :name,  presence: true
-  validates :email, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord::Base
+class User < ApplicationRecord
   has_many :microposts
   validates :name,  presence: true
   validates :email, presence: true
