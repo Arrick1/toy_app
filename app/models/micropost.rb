66b678d3318198b3dd14d8975612b98cb3fc0a8b
Arrick D <arrickdunlap@gmail.com>
2021-10-11 19:44:52 +0000

    remover micropost from user index

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to:user
   validates :content, length: { maximum: 140 }
   validates :name,  presence: true
   validates :email, presence: true
